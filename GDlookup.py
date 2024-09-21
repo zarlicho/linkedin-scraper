8522e5011ee4faa3ea2103054072eb5dbcddb8a1
zarlicho <fatihhurisqi1492005@gmail.com>
2024-09-21 19:30:33 +0700

    first commit

---
+++ GDlookup.py
@@ -0,0 +1,202 @@
+import re,requests,urllib.parse,json
+from seleniumbase import SB
+
+class LookUp:
+	def __init__(self):
+		self.sb = None
+		self.VIEW_NAME = "Dwayne View" # Enter VIEW_NAME here. OR leave it empty.
+		self.LinkedIN_LOGIN_EMAIL = "dg135862@gmail.com"
+		self.LinkedIN_LOGIN_PASSWORD = "$ystem@dmin97"
+		self.GLASSDOOR_LOGIN_EMAIL = "czgojueycxqdjnvzjr@tmmbt.net"
+		self.GLASSDOOR_PASSWORD = "czgojueycxqdjnvzjr@tmmbt.net"
+		self.INPUT_BASE_ID = 'appjvhsxUUz6o0dzo'
+		self.API_KEY = 'patQIAmVOLuXelY42.df469e641a30f1e69d29195be1c1b1362c9416fffc0ac17fd3e1a0b49be8b961'
+		self.Prospectus_Table = 'tblf4Ed9PaDo76QHH'
+		self.CRM_BASE_ID = 'appjvhsxUUz6o0dzo'
+		self.CRM_BASE_Prospectus_Tabke  = 'tblf4Ed9PaDo76QHH'
+		self.headers = {'Authorization': 'Bearer '+ self.API_KEY}
+		self.Post_Header = {'Authorization': 'Bearer '+ self.API_KEY,'Content-Type': 'application/json'}
+		self.AllRecordIds = []
+		self.GDrecords = []
+		
+
+	def updateCrm(self,company,gdurl):
+		print("function calling",gdurl)
+		CompanyRecordIDURL = "https://api.airtable.com/v0/" + self.CRM_BASE_ID + "/" + self.CRM_BASE_Prospectus_Tabke + "?filterByFormula=%7BCompany+Name%7D%3D%27" + urllib.parse.quote_plus(company) + "%27"
+		response = requests.request("GET", CompanyRecordIDURL, headers=self.headers).json()
+		try:
+			print(response)
+			CompanyRecordID = response["records"][0]["id"]		
+			# data = {"fields": {"Glassdoor URL": gdurl,}}
+
+			json_data = json.dumps({"fields": {"Glassdoor URL": gdurl}})
+			response = requests.request("PATCH", f"https://api.airtable.com/v0/{self.CRM_BASE_ID}/{self.CRM_BASE_Prospectus_Tabke}/{CompanyRecordID}", headers=self.Post_Header, data=json_data)
+			print(" " * 9, "CRM PATHCING STATUS: ", response.status_code)
+			print(" " * 9, f"UPDATING {company} TO CRM DONE!")
+			if response.status_code != 200:
+				print("Error patching Airtable record:", response.json())
+		except Exception as e:
+			print(e)
+			return
+
+	def getInputCompanyTable(self):
+		offset = ''
+		while 1:
+			CompanyTableURL = 'https://api.airtable.com/v0/' + self.INPUT_BASE_ID + '/' + self.Prospectus_Table
+			if len(self.VIEW_NAME) > 1:
+				OutputTable = requests.get(CompanyTableURL, headers=self.headers, params={'offset': offset, 'view': self.VIEW_NAME}).json()
+			else:
+				OutputTable = requests.get(CompanyTableURL, headers=self.headers, params={'offset': offset}).json()
+			for Records in OutputTable["records"]:
+				for recordsKey, recordsValue in Records.items():
+					if recordsKey == "fields":
+						SingleRecord = {}
+						try:
+							if "Glassdoor URL" not in list(recordsValue.keys()):
+								print(recordsValue['Company Name'])
+								try:
+									SingleRecord["Company Name"] = recordsValue["Company Name"]
+								except KeyError:
+									continue
+								try:
+									SingleRecord["HQ"] = recordsValue['HQ']
+								except KeyError:
+									continue
+								try:
+									SingleRecord["Website"] = recordsValue["Website (from Companies)"]
+								except Exception as e:
+									# print(f"{recordsValue['Company Name']} company website not found!")
+									continue
+								self.AllRecordIds.append(SingleRecord)
+						except KeyError:
+							print(f"{recordsValue['Company Name']} already exist!")
+							print(recordsValue['Glassdoor URL'])
+			try:
+				nextOffset = OutputTable["offset"]
+				offset = nextOffset
+			except KeyError:
+				break
+
+	def login_glassdoor(self):
+		self.sb.uc_open_with_reconnect("https://www.glassdoor.com/index.htm", 3)
+		for repeat in range(3): #repeat login for 3 times
+			print(f"trying login for {repeat} times")
+			try:
+				self.sb.wait_for_element_visible("#inlineUserEmail",timeout=4)
+				if self.sb.is_element_visible("#inlineUserEmail"):
+					self.sb.type("#inlineUserEmail", self.GLASSDOOR_LOGIN_EMAIL + "\n", timeout=3, retry=2)
+					self.sb.type("/html/body/div[3]/section[1]/div[2]/div/div/div[1]/div[1]/div/div/div/form/div[1]/div[1]/div/div[1]/input", self.GLASSDOOR_PASSWORD + "\n")
+					break
+			except:
+				continue
+			else:
+				print("Login failed cause element not found!")
+				continue
+			
+	def get_elements(self, selector, by):
+		try:
+			self.sb.wait_for_element_visible(selector, by=by, timeout=10)
+			if self.sb.is_element_visible(selector, by):
+				try:
+					return self.sb.find_elements(selector, by, limit=50)
+				except Exception as e:
+					print(f"Error finding elements {selector}: {e}")
+					return None
+			else:
+				print(f"Element {selector} not found!")
+				return None
+		except Exception as e:
+			print(f"Exception in get_elements for {selector}: {e}")
+			return None
+
+	def get_element(self,selector,by):
+		try:
+			self.sb.wait_for_element_visible(selector,by=by,timeout=3)
+			if self.sb.is_element_visible(selector,by):
+				try:
+					return self.sb.find_element(selector,by)
+				except Exception as e:
+					return f"error: {e}"
+			else:
+				return f"error {selector} not found!"
+		except Exception as e:
+			print(e)
+			pass
+		return None
+
+	def filterUrl(self,url,pat):
+		match = re.search(pat,url)
+		if match:
+			return match.group(1)
+	
+	def search_company(self):
+		for Records in self.AllRecordIds:
+
+			self.sb.open(f"https://www.glassdoor.com/Reviews/company-reviews.htm?typedKeyword={Records['Company Name']}&context=Review&sc.keyword={Records['Company Name']}")			
+			page = self.sb.get_current_url()
+			print(page)
+			if "Explore" not in page:
+				isfound = False
+				isCompany = self.get_element('//*[@id="MainCol"]/div/header/h1',by="xpath")			
+				if isCompany:
+					if f'Showing results for ' in isCompany.text:
+						paging = self.get_elements('//*[@class="page "]',by="xpath")
+						pageIndex = 0
+						if paging:
+							for pagenum in range(1,len(paging)+4):
+								pageIndex+=1
+							print("Page Total: ",pagenum)
+						else:
+							pageIndex=1
+						for pg in range(1,pageIndex+3):
+							companyList = self.get_elements('//*[@class="single-company-result module "]',by="xpath")
+							if companyList:
+								for x in range(1,len(companyList)+2):
+									gdurl = self.get_element(f'//*[@id="MainCol"]/div/div[{x}]/div/div[1]/div/div[2]/h2/a',by="xpath")					
+									compurl = self.get_element(f'//*[@id="MainCol"]/div/div[{x}]/div/div[1]/div/div[2]/div/p[2]/span/a',by="xpath")					
+									if gdurl and compurl:
+										print(Records['Website'][0])
+										print("company name: ",Records['Company Name'])
+										if self.filterUrl(compurl.get_attribute("href"),pat=r'^(?:https?://)?(?:www\.)?([^/]+)') == self.filterUrl(Records['Website'][0],pat=r'^(?:https?://)?(?:www\.)?([^/]+)'):
+											print(f"Found Glassdoor {Records['Company Name']} URL: ", gdurl.get_attribute("href"))
+											self.updateCrm(company=Records['Company Name'],gdurl=gdurl.get_attribute("href"))
+											self.GDrecords.append({"Company Name":Records['Company Name'],"GD URL":gdurl.get_attribute("href")})
+											isfound = True
+											break
+								if isfound:
+									break
+								self.sb.open(f'{page.split(".htm")[0]}_IP{pg+1}.htm')	
+								# print(f"{Records['Company Name']} Not Found!")							
+						if isfound == False:
+							print(f"{Records['Company Name']} not found")
+							self.updateCrm(company=Records['Company Name'],gdurl="\n")
+					else:
+						print(f"{Records['Company Name']} not found")
+						self.updateCrm(company=Records['Company Name'],gdurl="\n")
+				else:
+					if (self.filterUrl(url=page,pat=r'^(?:https?://)?(?:www\.)?[^/]+/([^/?]+)')) == "Overview":
+						compurl = self.get_element(selector='//*[@id="__next"]/div/div[1]/div[2]/main/div/div/div[1]/div/ul/li[1]/a',by="xpath")
+						if compurl:
+							if self.filterUrl(compurl.get_attribute("href"),pat=r'^(?:https?://)?(?:www\.)?([^/]+)') == self.filterUrl(Records['Website'][0],pat=r'^(?:https?://)?(?:www\.)?([^/]+)'):
+								print(f"Found Glassdoor {Records['Company Name']} URL: ", page)
+								print("company name: ",Records['Company Name'])
+								self.updateCrm(company=Records['Company Name'],gdurl=page)
+								self.GDrecords.append({"Company Name":Records['Company Name'],"GD URL":page})
+							else:
+								print(f"{Records['Company Name']} not found")
+								self.updateCrm(company=Records['Company Name'],gdurl="\n")
+						else:
+							print(f"{Records['Company Name']} not found")
+							self.updateCrm(company=Records['Company Name'],gdurl="\n")
+
+	def Main(self):
+		with SB(uc=True) as Sb:
+			self.sb = Sb
+			self.login_glassdoor()
+			self.getInputCompanyTable()
+			self.search_company()
+
+
+if __name__ == "__main__":
+	lkp = LookUp()
+	lkp.Main()
